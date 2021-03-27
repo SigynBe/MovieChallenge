a1e9a29bd573d048447e471d9f0332daa638be17
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2021-03-27 18:43:07 -0400

    Initial Commit

---
+++ MovieChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieChallenge
+//
+//  Created by Rebeca Pacheco on 27/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
